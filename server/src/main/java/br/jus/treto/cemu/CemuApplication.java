861750b8d9bb6128ff05093cb1c3039e5b57b4a7
Vilnei de Melo Rodrigues <vilnei@tre-to.jus.br>
2020-11-11 15:17:58 -0300

    @EnableSpringDataWebSupport para suport a .war

--- server/src/main/java/br/jus/treto/cemu/CemuApplication.java
+++ server/src/main/java/br/jus/treto/cemu/CemuApplication.java
@@ -2,12 +2,21 @@ package br.jus.treto.cemu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-public class CemuApplication {
+@EnableSpringDataWebSupport //  war tradicional
+public class CemuApplication   extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CemuApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CemuApplication.class);
 	}
 
 }
